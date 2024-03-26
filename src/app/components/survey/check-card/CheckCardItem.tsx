ddd4338994681bec1618bf97d2e456d6de691104
ElenaiDev <ec22761@qmul.ac.uk>
2024-03-26 23:29:01 +0000

    Add RatingCard component and update CheckCardItem component

--- src/app/components/survey/check-card/CheckCardItem.tsx
+++ src/app/components/survey/check-card/CheckCardItem.tsx
@@ -6,7 +6,7 @@ interface CheckCardItemProps {
 const CheckCardItem = ({ text }: CheckCardItemProps) => {
     return (
         <label className="flex items-center gap-4 font-medium cursor-pointer">
-            <input type="checkbox" className="
+            <input type="checkbox" name="text.replace(/\s+/g, '').toLowerCase()" className="
             w-6 h-6
             checkbox 
             checkbox-primary
