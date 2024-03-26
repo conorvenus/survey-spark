27c2e275be50cfd3fad9742265a9e1fb6f1d4000
ElenaiDev <ec22761@qmul.ac.uk>
2024-03-26 22:26:45 +0000

    Added a 404 Page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+    return (
+    <section className="h-[80vh] flex flex-col items-center justify-center">
+        <h1 className="text-primary font-bold text-5xl">404</h1>
+        <p className="text-2xl">Page Not Found</p>
+        <Link className="btn btn-outline btn-primary mt-4" href={"/"}>Go back home</Link>
+    </section>
+    )
+}
